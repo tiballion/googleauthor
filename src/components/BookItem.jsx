8aa226994ffe86d3f6c37d04e1bbff9cd14635e5
tiballion <timothee.ballion.33@gmail.com>
2023-01-28 00:57:05 +0100

    fixed bug auteur inconnu

--- src/components/BookItem.jsx
+++ src/components/BookItem.jsx
@@ -1,23 +1,26 @@
 const BookItem = ({ book }) => {
-  const handleClick = () => {
-    console.log(book)
-  }
-
   return (
     <>
       <div
-        className="h-60 p-2 bg-zinc-800 overflow-auto rounded-md flex flex-col items-center cursor-pointer"
+        className="h-60 p-2 bg-zinc-800 overflow-auto rounded-md flex flex-col justify-between items-center cursor-pointer border border-gray-700 hover:border-indigo-500"
         onClick={() => window.open(book.volumeInfo.infoLink)}
       >
         <h2 className="text-xl font-semibold text-center">
           {book.volumeInfo.title}
         </h2>
-        <p>
-          {book.volumeInfo.authors.map((author) => (
-            <span className="text-sm mx-1 rounded-md">{author}</span>
-          ))}
-        </p>
-        <p className="text-sm">{book.volumeInfo.publishedDate}</p>
+        <div className="flex flex-col items-center">
+          {book.volumeInfo.authors ? (
+            <p>
+              {book.volumeInfo.authors.map((author) => (
+                <span className="text-sm mx-1 rounded-md">{author}</span>
+              ))}
+            </p>
+          ) : (
+            <p className="text-sm">Auteur inconnu</p>
+          )}
+
+          <p className="text-sm">{book.volumeInfo.publishedDate}</p>
+        </div>
       </div>
     </>
   )

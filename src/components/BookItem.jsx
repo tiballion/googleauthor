768e32bd042de28002edf3b4c02fad7119c92c5b
tiballion <timothee.ballion.33@gmail.com>
2023-01-28 00:10:53 +0100

    changed style of book item

--- src/components/BookItem.jsx
+++ src/components/BookItem.jsx
@@ -1,7 +1,7 @@
 const BookItem = ({ book }) => {
   return (
     <>
-      <div className="h-40 bg-stone-500 overflow-auto rounded-md">
+      <div className="h-40 p-2 bg-zinc-800 overflow-auto rounded-md">
         <h3>{book.volumeInfo.title}</h3>
         <p>{book.volumeInfo.authors}</p>
         <p>{book.volumeInfo.publishedDate}</p>

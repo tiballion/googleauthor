94b94ac85811056b5d4601acf7da02744314abbf
tiballion <timothee.ballion.33@gmail.com>
2023-01-27 21:57:03 +0100

    removed app.css import, added react icns

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css"
 import Header from "./components/Header"
 import Search from "./components/Search"
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react"
+import { BsBook } from "react-icons/bs"
 
 const Header = () => {
   return (
     <header className="bg-neutral-800 p-3">
       <h1>Google Author</h1>
       <h3 className="pl-2 text-neutral-400">API de recherche Google</h3>
+      <BsBook />
     </header>
   )
 }

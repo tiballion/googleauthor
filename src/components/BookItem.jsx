f46df82219bd36a369023b60da9fbd5558e34164
tiballion <timothee.ballion.33@gmail.com>
2023-01-28 00:45:57 +0100

    added basic style on books & on click redirect

--- src/components/BookItem.jsx
+++ src/components/BookItem.jsx
@@ -1,10 +1,23 @@
 const BookItem = ({ book }) => {
+  const handleClick = () => {
+    console.log(book)
+  }
+
   return (
     <>
-      <div className="h-40 p-2 bg-zinc-800 overflow-auto rounded-md">
-        <h3>{book.volumeInfo.title}</h3>
-        <p>{book.volumeInfo.authors}</p>
-        <p>{book.volumeInfo.publishedDate}</p>
+      <div
+        className="h-60 p-2 bg-zinc-800 overflow-auto rounded-md flex flex-col items-center cursor-pointer"
+        onClick={() => window.open(book.volumeInfo.infoLink)}
+      >
+        <h2 className="text-xl font-semibold text-center">
+          {book.volumeInfo.title}
+        </h2>
+        <p>
+          {book.volumeInfo.authors.map((author) => (
+            <span className="text-sm mx-1 rounded-md">{author}</span>
+          ))}
+        </p>
+        <p className="text-sm">{book.volumeInfo.publishedDate}</p>
       </div>
     </>
   )
--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -3,7 +3,7 @@ import BookItem from "./BookItem"
 const Books = (books) => {
   return (
     <>
-      <div className="grid gap-4 grid-cols-5 grid-rows-2 p-4">
+      <div className="grid gap-4 p-4 md:grid-cols-2 md:grid-rows-5 lg:grid-cols-5 lg:grid-rows-2">
         {books.books.map((book) => (
           <BookItem key={book.id} book={book} />
         ))}

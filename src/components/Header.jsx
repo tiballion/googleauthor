53f59bd22a13893c5399be7c6066fd08251532ef
tiballion <timothee.ballion.33@gmail.com>
2023-01-28 00:49:44 +0100

    fixed header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,13 @@ import React from "react"
 const Header = () => {
   return (
     <header className="bg-zinc-800 p-3 m-2 rounded-md flex flex-col items-center">
-      <h1>Google Author </h1>
+      <h1 className="text-4xl">Google Author </h1>
       <h3 className="pl-2 text-neutral-400">
         API de recherche Google | Made by{" "}
         <a
           href="https://github.com/tiballion"
           target="_blank"
-          className="text-indigo-500"
+          className="text-indigo-500 hover:text-indigo-400"
         >
           Timothée
         </a>

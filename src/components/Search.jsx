f5336fd0c7e1de1d4e367c29ea7888a8e6910cca
tiballion <timothee.ballion.33@gmail.com>
2023-01-28 01:43:51 +0100

    added submit type

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -74,7 +74,10 @@ const Search = () => {
               onChange={handleChange}
             />
           </label>
-          <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 m-1 border border-gray-400 rounded shadow">
+          <button
+            type="submit"
+            className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 m-1 border border-gray-400 rounded shadow"
+          >
             <AiOutlineSearch size={22} />
           </button>
         </form>

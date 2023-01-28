c51e2589e04c4e1e78916969d518861836085fa2
tiballion <timothee.ballion.33@gmail.com>
2023-01-28 01:44:56 +0100

    header is now responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ import React from "react"
 
 const Header = () => {
   return (
-    <header className="bg-zinc-800 p-3 m-2 rounded-md flex flex-col items-center">
+    <header className="bg-zinc-800 p-3 m-2 rounded-md flex flex-col items-center text-center">
       <h1 className="text-4xl">Google Author </h1>
-      <h3 className="pl-2 text-neutral-400">
+      <h3 className="pl-2 text-neutral-400 text-center">
         API de recherche Google | Made by{" "}
         <a
           href="https://github.com/tiballion"

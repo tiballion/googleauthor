49d5f52f9685ad87d661f87f15bd94b585585572
tiballion <timothee.ballion.33@gmail.com>
2023-01-30 11:00:39 +0100

    changed to es7 syntax

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,4 +1,24 @@
+import React from "react"
 import BookItem from "./BookItem"
+
+class Books extends React.Component {
+  render() {
+    const { books } = this.props
+    return (
+      <>
+        <div className="grid gap-4 p-4 md:grid-cols-2 md:grid-rows-5 lg:grid-cols-5 lg:grid-rows-2">
+          {books.map((book) => (
+            <BookItem key={book.id} book={book} />
+          ))}
+        </div>
+      </>
+    )
+  }
+}
+
+export default Books
+
+/*import BookItem from "./BookItem"
 
 const Books = (books) => {
   return (
@@ -12,4 +32,4 @@ const Books = (books) => {
   )
 }
 
-export default Books
+export default Books*/

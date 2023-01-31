fe951589593a1178482e2fe74f177d1dcf734124
tiballion <timothee.ballion.33@gmail.com>
2023-01-31 09:23:40 +0100

    moved footer to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,22 @@
 import React from "react"
+import { AiOutlineGithub } from "react-icons/ai"
+import { BsCodeSlash } from "react-icons/bs"
 
 const Header = () => {
   return (
     <header className="bg-zinc-800 p-3 m-2 rounded-md flex flex-col items-center text-center shadow-md">
       <h1 className="text-4xl">Google Author </h1>
-      <h3 className="pl-2 text-neutral-400 text-center">
-        API de recherche Google
-      </h3>
+      <div className="flex justify-center align-middle items-center">
+        <button>
+          <AiOutlineGithub size={28} />
+        </button>
+        <h3 className="px-2 text-neutral-400 text-center">
+          API de recherche Google
+        </h3>
+        <button>
+          <BsCodeSlash size={28} />
+        </button>
+      </div>
     </header>
   )
 }

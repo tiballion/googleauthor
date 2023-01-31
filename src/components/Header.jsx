5f8b633fb0b05812c744c50074f8fae4fe30c6af
tiballion <timothee.ballion.33@gmail.com>
2023-01-31 09:03:33 +0100

    removed github link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,14 +5,7 @@ const Header = () => {
     <header className="bg-zinc-800 p-3 m-2 rounded-md flex flex-col items-center text-center shadow-md">
       <h1 className="text-4xl">Google Author </h1>
       <h3 className="pl-2 text-neutral-400 text-center">
-        API de recherche Google | Made by{" "}
-        <a
-          href="https://github.com/tiballion"
-          target="_blank"
-          className="text-indigo-500 hover:text-indigo-400"
-        >
-          Timothée
-        </a>
+        API de recherche Google
       </h3>
     </header>
   )

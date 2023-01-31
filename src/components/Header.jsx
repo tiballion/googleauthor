932acb3647122e82416983504144c1d56e19f340
tiballion <timothee.ballion.33@gmail.com>
2023-01-31 09:32:17 +0100

    finished header links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,15 +7,15 @@ const Header = () => {
     <header className="bg-zinc-800 p-3 m-2 rounded-md flex flex-col items-center text-center shadow-md">
       <h1 className="text-4xl">Google Author </h1>
       <div className="flex justify-center align-middle items-center">
-        <button>
+        <a href="https://github.com/tiballion" target="_blank">
           <AiOutlineGithub size={28} />
-        </button>
+        </a>
         <h3 className="px-2 text-neutral-400 text-center">
           API de recherche Google
         </h3>
-        <button>
+        <a href="https://github.com/tiballion/googleauthor" target="_blank">
           <BsCodeSlash size={28} />
-        </button>
+        </a>
       </div>
     </header>
   )

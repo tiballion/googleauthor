7a15694abe2c348d73be7ab5a3fcb3342e36206b
tiballion <timothee.ballion.33@gmail.com>
2023-01-31 09:04:30 +0100

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header"
 import Search from "./components/Search"
+import Footer from "./components/Footer"
 
 function App() {
   return (
     <>
       <Header />
       <Search />
+      <Footer />
     </>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,19 @@
 import React, { Component } from "react"
+import { AiOutlineGithub } from "react-icons/ai"
+import { BsCodeSlash } from "react-icons/bs"
 
 export class Footer extends Component {
   render() {
-    return <div>Footer</div>
+    return (
+      <footer className="bg-zinc-800 text-center fixed inset-x-0 bottom-0 p-4 m-2 rounded-md shadow-md align-middle justify-center">
+        <button>
+          <AiOutlineGithub size={32} />
+        </button>
+        <button>
+          <BsCodeSlash size={32} />
+        </button>
+      </footer>
+    )
   }
 }
 

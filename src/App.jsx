1ad183faf8cf6fee1fb6a1869eb70ff394a8bc81
tiballion <timothee.ballion.33@gmail.com>
2023-01-31 09:24:18 +0100

    removed footer import

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import Header from "./components/Header"
 import Search from "./components/Search"
-import Footer from "./components/Footer"
 
 function App() {
   return (
     <>
       <Header />
       <Search />
-      <Footer />
     </>
   )
 }

413bc2ecee2bcd8c2cab9c8912e8d2c5702760fe
tiballion <timothee.ballion.33@gmail.com>
2023-01-31 08:28:20 +0100

    added header box shadow

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const Header = () => {
   return (
-    <header className="bg-zinc-800 p-3 m-2 rounded-md flex flex-col items-center text-center">
+    <header className="bg-zinc-800 p-3 m-2 rounded-md flex flex-col items-center text-center shadow-md">
       <h1 className="text-4xl">Google Author </h1>
       <h3 className="pl-2 text-neutral-400 text-center">
         API de recherche Google | Made by{" "}

828d6a3ae27877a14aa27029602b4d0a52f2ce95
tiballion <timothee.ballion.33@gmail.com>
2023-03-03 10:58:23 +0100

    removed unused fragment

--- src/components/Books.tsx
+++ src/components/Books.tsx
@@ -7,12 +7,10 @@ interface BooksProps {
 
 export const Books = ({ books }: BooksProps) => {
   return (
-    <>
-      <div className="grid gap-4 p-4 md:grid-cols-2 md:grid-rows-5 lg:grid-cols-5 lg:grid-rows-2">
-        {books.map((book: Book) => (
-          <BookItem key={book.id} book={book} />
-        ))}
-      </div>
-    </>
+    <div className="grid gap-4 p-4 md:grid-cols-2 md:grid-rows-5 lg:grid-cols-5 lg:grid-rows-2">
+      {books.map((book: Book) => (
+        <BookItem key={book.id} book={book} />
+      ))}
+    </div>
   )
 }

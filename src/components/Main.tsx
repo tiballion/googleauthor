f53c888892f9274567666d4313a1ed4c50d2e38b
tiballion <timothee.ballion.33@gmail.com>
2023-03-03 13:14:12 +0100

    removed unused import

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, SyntheticEvent } from "react"
+import React, { useState, useEffect } from "react"
 import axios from "axios"
 import { Book } from "../types"
 import { Books, Pagination, Search } from "./components"
